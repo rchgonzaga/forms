40fa9f3f0338bb39ed064dd365f6648cd105a872
Rafael Gonzaga <rchgonzaga@gmail.com>
2017-11-15 20:58:25 -0200

    updating

---
+++ src/components/post_index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+
+class PostIndex extends Component {
+
+    render() {
+        return (
+            <div>
+                Post Index
+            </div>
+        )
+    }
+}
+
+export default PostIndex
+
+// function mapDispathToProps(dispath) {
+//     // state that you want to make available for this component
+//     return bindActionCreators({ XACTION }, dispath)
+// }
+
+// export default connect(null, mapDispathToProps)(PostIndex)
