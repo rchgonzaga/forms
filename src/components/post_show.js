512be3c0c4b57922fabc0baaa87dfd08a291d395
Rafael Gonzaga <rchgonzaga@gmail.com>
2017-11-18 19:45:28 -0200

    updating

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { fetchWeather } from '../actions/index'
+import { Link } from 'react-router-dom'
+import { fetchPost } from '../actions'
 
 
 class PostShow extends Component {
 
+    componentDidMount(){
+        const {id} = this.props.match.params
+        console.log(id)
+        this.props.fetchPost(id)
+    }
+
     render() {
+
+        const {post} = this.props
+
+        if(!post){
+            return <div>loading...</div>
+        }
+
         return (
             <div>
-                PostShow
+                <Link to="/">Back to index</Link>
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         )
     }
 }
 
-export default PostShow 
+function mapStateToProps({posts}, ownProps) {
+    return { post: posts[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, {fetchPost})(PostShow)
 
 // function mapDispathToProps(dispath) {
 //     return bindActionCreators({ fetchWeather }, dispath)
