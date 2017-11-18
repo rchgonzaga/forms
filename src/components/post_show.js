38700efd8d263454db17877675ee1d079765f142
Rafael Gonzaga <rchgonzaga@gmail.com>
2017-11-18 19:58:56 -0200

    updating

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { fetchPost } from '../actions'
+import { fetchPost, deletePost } from '../actions'
 
 
 class PostShow extends Component {
@@ -10,6 +10,13 @@ class PostShow extends Component {
         const {id} = this.props.match.params
         console.log(id)
         this.props.fetchPost(id)
+    }
+
+    onDeleteClick(){
+        const { id } = this.props.match.params
+        this.props.deletePost(id, () => {
+            this.props.history.push('/')
+        })
     }
 
     render() {
@@ -23,6 +30,9 @@ class PostShow extends Component {
         return (
             <div>
                 <Link to="/">Back to index</Link>
+                <button 
+                    className="btn btn-danger pull-xs-right"
+                    onClick={this.onDeleteClick.bind(this)} >Delete</button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -35,7 +45,7 @@ function mapStateToProps({posts}, ownProps) {
     return { post: posts[ownProps.match.params.id]}
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostShow)
+export default connect(mapStateToProps, { fetchPost, deletePost})(PostShow)
 
 // function mapDispathToProps(dispath) {
 //     return bindActionCreators({ fetchWeather }, dispath)
